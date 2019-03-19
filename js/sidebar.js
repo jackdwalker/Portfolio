05be24f32fed61ecc4045f5e48ab0e13f84ab0a3
Jack Walker <jwrc@protonmail.com>
2019-03-20 09:22:06 +1000

    Made sidebar .js dry.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,11 @@
 /* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-    document.getElementById("mySidepanel").style.width = (window.innerWidth / 2) + "px";
-    document.getElementById("myOpenbtn").style.display = "none";
+function openNav(panel,button) {
+  document.getElementById(panel).style.width = (window.innerWidth / 2) + "px";
+  document.getElementById(button).style.display = "none";
 }
-  
+
 /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-    document.getElementById("myOpenbtn").style.display = "inline-block";
+function closeNav(panel, button) {
+  document.getElementById(panel).style.width = "0";
+  document.getElementById(button).style.display = "inline-block";
 }
