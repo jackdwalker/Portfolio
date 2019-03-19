90ac826ab7cd94956f1ff7da6807ae1b1e8f3190
Jack Walker <jwrc@protonmail.com>
2019-03-19 18:41:13 +1000

    Finally made my sidebar work. Made some hacky javascript to get it to work.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,11 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("mySidepanel").style.width = (window.innerWidth / 2) + "px";
+    document.getElementById("myOpenbtn").style.display = "none";
 }
   
 /* Set the width of the sidebar to 0 (hide it) */
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("myOpenbtn").style.display = "inline-block";
 }
