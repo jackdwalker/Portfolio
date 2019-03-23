3b3a166076907235e20cd440b59005963fd16bf4
Jack Walker <jwrc@protonmail.com>
2019-03-23 23:47:14 +1000

    Added some content images

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,15 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav(panel,button) {
-  document.getElementById(panel).style.width = (window.innerWidth / 2) + "px";
-  document.getElementById(button).style.display = "none";
+  if (innerWidth > "415px") {
+    document.getElementById(panel).style.width = (window.innerWidth / 2) + "px";
+    document.getElementById(button).style.display = "none";
+  }
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav(panel, button) {
-  document.getElementById(panel).style.width = "0";
-  document.getElementById(button).style.display = "inline-block";
+  if (innerWidth > "415px") {
+    document.getElementById(panel).style.width = "0";
+    document.getElementById(button).style.display = "inline-block";
+  }
 }
